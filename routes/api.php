4731750309fa5a8f084f12d92e31b0937c5d530f
ahmadakbar00 <ahmadhiz2000@gmail.com>
2022-07-31 11:57:43 +0700

    REST API

--- routes/api.php
+++ routes/api.php
@@ -9,9 +9,27 @@ use App\Http\Controllers\BookController;
 //     return $request->user();
 // });
 
+// Route::post('/{path}', function() {
+//   return response('', 200)
+//       ->header(
+//         'Access-Control-Allow-Headers', 
+//         'Origin, Content-Type, Content-Range, Content-Disposition, Content-Description, X-Auth-Token, X-Requested-With')
+//       ->header('Access-Control-Allow-Methods', 'POST, GET, PUT, OPTIONS, DELETE')
+//       ->header('Access-Control-Allow-Origin','http://127.0.0.1:8000')
+//       ->header('Access-Control-Allow-Credentials',' true');
+// })->where('path', '.*');
+
 Route::get('/buku', [BookController::class, 'index']); //mengambil data
 
-Route::post('/buku/store', [BookController::class, 'store']); //menyimpan data
+Route::post('/buku/store', [BookController::class, 'store'], function(){
+    return response('', 200)
+      ->header(
+        'Access-Control-Allow-Headers', 
+        'Origin, Content-Type, Content-Range, Content-Disposition, Content-Description, X-Auth-Token, X-Requested-With')
+      ->header('Access-Control-Allow-Methods', 'POST, GET, PUT, OPTIONS, DELETE')
+      ->header('Access-Control-Allow-Origin','http://127.0.0.1:8000')
+      ->header('Access-Control-Allow-Credentials',' true');
+}); //menyimpan data
 Route::get('/buku/show/{id}', [BookController::class, 'show']); //mengambil data by id (detail data)
 Route::put('/buku/update/{id}', [BookController::class, 'update']); //update data
 Route::delete('/buku/destroy/{id}', [BookController::class, 'destroy']); //hapus data
